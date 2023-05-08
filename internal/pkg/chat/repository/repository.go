e8ca9df3286342a859611efad55991ecf6193920
Kichikova <90607035+Kichikova@users.noreply.github.com>
2023-05-09 00:46:44 +0300

    a

--- internal/pkg/chat/repository/repository.go
+++ internal/pkg/chat/repository/repository.go
@@ -1,33 +1,34 @@
 package repository
 
 import (
-	"gorm.io/gorm"
-
-	dataStruct "github.com/staurran/messengerKR.git/internal/app/ds"
+	"fmt"
+	"github.com/staurran/messengerKR.git/internal/app/constProject"
+	"github.com/staurran/messengerKR.git/internal/app/ds"
 	"github.com/staurran/messengerKR.git/internal/app/structs"
+	"gorm.io/gorm"
 )
 
 type ChatRepository struct {
 	db *gorm.DB
 }
 
-func (r *ChatRepository) CreateChat(chat dataStruct.Chat) error {
+func (r *ChatRepository) CreateChat(chat ds.Chat) error {
 	err := r.db.Create(&chat).Error
 	return err
 }
 
-func (r *ChatRepository) SaveChatUser(user dataStruct.ChatUser) error {
+func (r *ChatRepository) SaveChatUser(user ds.ChatUser) error {
 	err := r.db.Create(&user).Error
 	return err
 }
 
-func (r *ChatRepository) DeleteChatUser(user dataStruct.ChatUser) error {
+func (r *ChatRepository) DeleteChatUser(user ds.ChatUser) error {
 
-	err := r.db.First(&user, "user_id =? AND chat_id =?", user.UserId, user.ChatID).Error
+	err := r.db.First(&user, "user_id =? AND chat_id =?", user.UserID, user.ChatID).Error
 	if err != nil {
 		return err
 	}
-	err = r.db.Delete(&dataStruct.ChatUser{}, "user_id =? AND chat_id =?", user.UserId, user.Photo).Error
+	err = r.db.Delete(&ds.ChatUser{}, "user_id =? AND chat_id =?", user.UserID, user.ChatID).Error
 	return err
 }
 
@@ -38,4 +39,36 @@ func (r *ChatRepository) GetChats(userId uint) ([]structs.ChatStruct, error) {
 		Joins("Join chatuser cu ON c.id = cu.chat_id").
 		Where("cu.user_id=?", userId).Find(&chats).Error
 	return chats, err
+}
+
+func (r *ChatRepository) ChangeChat(chatInp ds.Chat) error {
+	chatDB := &ds.Chat{}
+	err := r.db.First(chatDB, "id = ?", chatInp.ID).Error // find product with code D42
+	if err != nil {
+		return err
+	}
+	if chatInp.Name != "" {
+		chatDB.Name = chatInp.Name
+	}
+	if chatInp.Avatar != "" {
+		chatDB.Avatar = chatInp.Avatar
+	}
+	if chatInp.Description != "" {
+		chatDB.Description = chatInp.Description
+	}
+	err = r.db.Save(&chatDB).Error
+	return err
+}
+
+func (r *ChatRepository) CheckAdmin(userId, chatId uint) error {
+	chatUser := &ds.ChatUser{}
+	err := r.db.First(chatUser, "user_id = ? AND chat_id = ?", userId, chatId).Error
+	if err != nil {
+		return err
+	}
+	if chatUser.ChatRole == constProject.ChatAdmin {
+		return nil
+	}
+	err = fmt.Errorf("user is not an admin")
+	return err
 }

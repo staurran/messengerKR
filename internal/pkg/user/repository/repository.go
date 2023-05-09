d233182fa0b4a8f91407f92de0ef4609e0a329ef
Kichikova <90607035+Kichikova@users.noreply.github.com>
2023-05-09 14:29:00 +0300

    даша супер

--- internal/pkg/user/repository/repository.go
+++ internal/pkg/user/repository/repository.go
@@ -11,11 +11,6 @@ type UserRepository struct {
 	db *gorm.DB
 }
 
-func (r *UserRepository) CreateUser(user *ds.User) error {
-	err := r.db.Create(user).Error
-	return err
-}
-
 func (r *UserRepository) Login(user *ds.User) error {
 	user_db := ds.User{}
 	err := r.db.Model(&ds.User{}).Where("username = ?", user.Username).Take(&user_db).Error
@@ -38,15 +33,6 @@ func (r *UserRepository) GetUserByID(id uint) (*ds.User, error) {
 	}
 
 	return user, nil
-}
-
-func (r *UserRepository) GetIdByUsername(username string) (uint, error) {
-	user := &ds.User{}
-	err := r.db.First(user, "username = ?", username).Error
-	if err != nil {
-		return 0, err
-	}
-	return user.Id, nil
 }
 
 func (r *UserRepository) ChangeUser(userInp ds.User) error {
@@ -74,13 +60,6 @@ func (r *UserRepository) ChangeUser(userInp ds.User) error {
 func (r *UserRepository) GetContacts(userId uint) (contacts []ds.User, err error) {
 	err = r.db.Find(&contacts, "user_id = ?", userId).Error
 	return
-}
-
-func (r *UserRepository) GetUserIdByPhone(phone string) (userId uint, err error) {
-	user := &ds.User{}
-	err = r.db.First(user, "phone = ?", phone).Error
-
-	return user.Id, err
 }
 
 func (r *UserRepository) CreateContact(contact ds.Contact) error {

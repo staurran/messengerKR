ba818191abac23f03c1244fb1ecff709ef1ba8f2
Staurran <staurran@gmail.com>
2023-05-11 16:27:27 +0300

    continue work

--- internal/pkg/user/repository.go
+++ internal/pkg/user/repository.go
@@ -1,1 +1,8 @@
 package user
+
+import "github.com/staurran/messengerKR.git/internal/app/ds"
+
+type IUserRepository interface {
+	GetUserIdByPhone(phone string) (uint, error)
+	CreateContact(contact ds.Contact) error
+}
--- internal/pkg/user/usecase/usecase.go
+++ internal/pkg/user/usecase/usecase.go
@@ -1,1 +1,23 @@
 package usecase
+
+import (
+	"github.com/staurran/messengerKR.git/internal/app/ds"
+	"github.com/staurran/messengerKR.git/internal/pkg/user"
+)
+
+type UserUseCase struct {
+	UserRepo user.IUserRepository
+}
+
+func (uc *UserUseCase) CreateContact(userId uint, phone string) error {
+	contactId, err := uc.UserRepo.GetUserIdByPhone(phone)
+	if err != nil {
+		return err
+	}
+	contectRow := ds.Contact{UserID: userId, ContactID: contactId}
+	err = uc.UserRepo.CreateContact(contectRow)
+	if err != nil {
+		return err
+	}
+	return nil
+}
